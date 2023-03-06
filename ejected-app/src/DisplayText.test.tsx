dc7e9e181b8f95141bc29b066df0fa34da4d9102
pawel laptop <grzelek9@gmail.com>
2023-03-06 21:21:13 +0000

    test

--- ejected-app/src/DisplayText.test.tsx
+++ ejected-app/src/DisplayText.test.tsx
@@ -1,39 +1,76 @@
 import React from "react"
-import {render, fireEvent, getByTestId} from "@testing-library/react"
+import {render, fireEvent, cleanup, waitFor} from "@testing-library/react"
 import DisplayText from "./DisplayText"
 import "@testing-library/jest-dom/extend-expect"
 import { getValue } from "@testing-library/user-event/dist/utils"
-/*
+
 describe("test component DisplayText", ()=>{
+    const userFullName = "Jack Tester";
+    const getUserFullnameMock = (
+        username: string): [Promise<string>, jest.Mock<Promise<string>, [string]>] => {
+            const promise = new Promise<string>((res, rej) => {
+                res(userFullName)
+            });
+            const getUserFullname = jest.fn(
+                async (username: string): Promise<string> =>{
+                    return promise;
+                }
+            )
+            return [promise, getUserFullname]
+        }
+       
     it("no any issue",()=>{
-        const{baseElement} = render(<DisplayText />)
-        console.log(baseElement.innerHTML)
+        const username = "testuser"
+        const [promise, getUserFullname] = getUserFullnameMock(username)
+        const{baseElement} = render(<DisplayText getUserFullname={getUserFullname}/>)
+        //console.log(baseElement.innerHTML)
         expect(baseElement).toBeInTheDocument()
     })
+     
     it("recived text from field", ()=>{
-        const testuser = "testuser";
-        const {getByTestId} = render(<DisplayText />)
+        const username = "testuser";
+        const [promise, getUserFullname] = getUserFullnameMock(username)
+        const {getByTestId} = render(<DisplayText getUserFullname={getUserFullname} />)
         const input = getByTestId("user-in");
-        fireEvent.change(input, {target: {value:testuser} })
+        fireEvent.change(input, {target: {value:username} })
         expect(input).toBeInTheDocument();
-        expect(input).toHaveValue(testuser)
+        expect(input).toHaveValue(username)
     })
-    it("displayed welcome text", ()=>{
-        const testuser = "testuser";
-        const msg = `hello in react test, ${testuser}!`;
-        const {getByTestId} = render(<DisplayText />)
+    
+    it("displayed welcome text", async ()=>{
+        const username = "testuser";
+        const [promise, getUserFullname] = getUserFullnameMock(username)
+
+        const msg = `hello in react test, ${userFullName}!`;
+        const {getByTestId} = render(<DisplayText getUserFullname={getUserFullname}  />)
         const input = getByTestId("user-in");
         const label = getByTestId("final-msg");
-        fireEvent.change(input, {target: {value:testuser} })
+        fireEvent.change(input, {target: {value:username} })
         const btn = getByTestId("input-submit")
         fireEvent.click(btn)
-       
-        expect(label).toBeInTheDocument();
+        
+        expect(label).toMatchSnapshot();
+        await waitFor(() => promise);
         expect(label.innerHTML).toBe(msg)
     })
-    it("jest zgodny z migawka", () => {
-        const {baseElement} = render(<DisplayText />)
-        expect(baseElement).toMatchSnapshot();
+    /*
+    it("jest zgodny z migawka", async() => {
+        const username = "testuser";
+        const [promise, getUserFullname] = getUserFullnameMock(username)
+
+        const msg = `hello in react test, ${userFullName}!`;
+        const {getByTestId} = render(<DisplayText getUserFullname={getUserFullname}  />)
+        const input = getByTestId("user-in");
+        const label = getByTestId("final-msg");
+        fireEvent.change(input, {target: {value:username} })
+        const btn = getByTestId("input-submit")
+        fireEvent.click(btn)
+        
+        expect(label).toMatchSnapshot();
+        await waitFor(() => promise);
+        expect(label.innerHTML).toBe(msg)
     })
+    */
+    
 })
-*/
+
